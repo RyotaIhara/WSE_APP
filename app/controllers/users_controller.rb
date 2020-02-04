65a8b2e472f3fec5cfb158957e994c5abb0f2f5c
program <ihara0531@gmail.com>
2020-02-05 00:11:22 +0900

    complete user validate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to :users, notice: "ユーザーを登録しました。"
+            redirect_to @user, notice: "ユーザーを登録しました。"
         else
             render "new"
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :name, 
+        presence: true,
+        uniqueness: true,
+        length: { maximum: 30 }
+
+    validates :full_name,
+        presence: true,
+        length: { maximum: 30 }
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,
+        presence: true,
+        uniqueness: true,
+        length: { maximum: 50 },
+        format: { with: VALID_EMAIL_REGEX }
+
+    VALID_PASSWORD_REGEX = /(?=.*\d+.*)(?=.*[a-zA-Z]+.*).*[!@#$%]+.*/
+    validates :password,
+        presence: true,
+        length: { minimum: 8, maximum: 30 },
+        format: { with: VALID_PASSWORD_REGEX }
+
 end
