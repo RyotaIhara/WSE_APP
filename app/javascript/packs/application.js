3a828d9587c1dc86cfcd159c8a6507837d3fed6a
program <ihara0531@gmail.com>
2020-02-11 01:36:11 +0900

    design nav image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('jquery')
 
 import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
